16410d959bda18b2c3f96a5df36d60f77baacae6
Sathish <42292676+satheeshsoft@users.noreply.github.com>
2024-03-18 02:48:05 +0530

    Add files via upload

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'BirdSetup.exe'
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  file         = $fileLocation
+  softwareName  = 'Bird'
+  validExitCodes= @(0, 3010, 1641)
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'  
+}
+Install-ChocolateyInstallPackage @packageArgs
