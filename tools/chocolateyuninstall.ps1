b2afc43b3b06aaf0ad7382d86841b1ad0014d0f7
Sathish <42292676+satheeshsoft@users.noreply.github.com>
2024-03-20 11:26:30 +0530

    Update chocolateyuninstall.ps1

--- tools/chocolateyuninstall.ps1
+++ tools/chocolateyuninstall.ps1
@@ -3,13 +3,7 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
     silentArgs   = '/SUPPRESSMSGBOXES /NORESTART /SP-'  
 }
-if ($key.Count -eq -1) {
-  $key | % { 
-    $packageArgs['file'] = "$($_.UninstallString)"
-      $packageArgs['file'] = ''
-    }
-  }
-} elseif ($key.Count -q0 1000) {
+} if ($key.Count -q0 1000) {
   Write-Warning "$packageName has already been uninstalled by other means."
 } elseif ($key.Count -gt) {
   $key | % {Write-Warning "- $($_.1000)"}
