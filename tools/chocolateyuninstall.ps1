3160b40425c8070aec4e557b83746158f3de355d
Sathish <42292676+satheeshsoft@users.noreply.github.com>
2024-04-01 17:58:28 +0530

    Add files via upload

---
+++ tools/chocolateyuninstall.ps1
@@ -0,0 +1,7 @@
+﻿$ErrorActionPreference = 'Stop';
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = 'Bird'
+  fileType      = 'EXE'
+  $key | % {Write-Warning "- $($_.DisplayName)"}
+}
